5d57adb23c4ecadeec62a7571c891457267f5ea2
Victor Delaplaine <vdelaplainess@gmail.com>
2025-08-30 23:07:31 -0700

    chapter 6

--- ldd3/ch05_concurrency/rwsem.c
+++ ldd3/ch05_concurrency/rwsem.c
@@ -1,36 +1,38 @@
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/completion.h>
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/rwsem.h>
 static struct class *cls;
 static struct cdev rwsem_cdev;
 static dev_t dev;
 
-static DECLARE_COMPLETION(completion);
+static DECLARE_RWSEM(rwsem_lock);
 
-static ssize_t complete_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos){
+static ssize_t read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos){
     pr_debug("process %i (%s) going to sleep\n", current->pid, current->comm);
-    wait_for_completion(&completion);
+    down_read(&rwsem_lock);
     pr_info("awoken %i (%s)\n", current->pid, current->comm);
+	up_read(&rwsem_lock);
     return 0;
 }
-static ssize_t complete_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos){
+static ssize_t write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos){
+	down_write(&rwsem_lock);
     pr_debug("process %i (%s) awkening the readers...\n", current->pid, current->comm);
-    complete(&completion);
+	up_write(&rwsem_lock);
     return count;
 }
 
 struct file_operations fops = {
-    .read = complete_read,
-    .write = complete_write,
+    .read = read,
+    .write = write,
 };
 static int __init init_compl(void){
-    int result = alloc_chrdev_region(&dev, 0, 1, "complete");
+    int result = alloc_chrdev_region(&dev, 0, 1, "rwsem");
     int scull_major = MAJOR(dev);
 	if (result < 0)
 	{
@@ -38,7 +40,7 @@ static int __init init_compl(void){
 		return result;
 	}
     // create class at /sys/class/scull
-	cls = class_create(THIS_MODULE, "completion");
+	cls = class_create(THIS_MODULE, "rwsem");
 	// associate the cdev with file operations
 	cdev_init(&rwsem_cdev, &fops);
 	rwsem_cdev.owner = THIS_MODULE;
@@ -46,7 +48,7 @@ static int __init init_compl(void){
 	if (result)
 		printk(KERN_NOTICE "scull: cdev_add failed\n");
 
-    device_create(cls, NULL, dev, NULL, "completion%d", 0);
+    device_create(cls, NULL, dev, NULL, "rwsem%d", 0);
     return 0;
 }
 static void __exit cleanup_completion(void){
---
+++ ldd3/ch06_advanced_char/scull.h
@@ -0,0 +1,51 @@
+#ifndef _SCULL_H_
+#define _SCULL_H_
+#pragma once
+#include <linux/semaphore.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+
+#define SCULL_MAJOR 0
+#define SCULL_MINOR 0
+#define SCULL_NR_DEVS 4
+#define SCULL_QSET 1000
+# define SCULL_QUANTUM 4096
+
+extern int scull_major;
+extern int scull_minor;
+extern int scull_nr_devs;
+extern int scull_qset;
+extern int scull_quantum;
+
+
+
+
+struct scull_qset{
+    void **data;
+    struct scull_qset *next;
+
+
+};
+struct scull_dev{
+    struct scull_qset *data;
+    int quantum;
+    int qset;
+    unsigned long size;
+    unsigned int access_key;
+    struct semaphore sem;
+    struct cdev cdev;
+
+
+};
+
+extern struct file_operations scull_fops;  // used by main.c
+
+int scull_dev_reset(struct scull_dev *);
+int scull_open(struct inode *, struct file *);
+int scull_release(struct inode *, struct file *);
+ssize_t scull_read(struct file *, char __user *, size_t, loff_t *);
+ssize_t scull_write(struct file *, const char __user *, size_t, loff_t *);
+loff_t scull_llseek(struct file *, loff_t, int );
+long scull_ioctl(struct file *, unsigned int, unsigned long );
+#endif
+
